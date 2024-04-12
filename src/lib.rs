955a41b47b4f234c173eddcf679d670a2455cf26
Michel Pastor <shellixyz@gmail.com>
2024-04-12 16:16:55 +0100

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
-pub trait SpiExt {}
+#![no_std]
+
+use frequency::Hertz;
+
+pub trait SpiExt {
+	type SetFrequencyError;
+	fn frequency(&self) -> Hertz;
+	fn set_frequency(&mut self, freq: Hertz) -> Result<(), Self::SetFrequencyError>;
+}
